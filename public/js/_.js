3db97a6292972b8be50aaa63e8f39fe75907be7b
Kinslow <simon.kinslow@aa7778.weu.grey.global>
2018-06-07 09:39:09 +0100

    Sound events and timers within class using bind(this);
    General code clean up

--- public/js/_.js
+++ public/js/_.js
@@ -11,14 +11,66 @@ class SoundFX
 	create(params)
 	{
 		this.instanceClass 	= params.instanceClass;
-		this.loop 			= params.loop;
-		this.playCount 		= params.playCount;
-		this.playMax 		= params.playMax;
+		this.loop 			= params.loop || false;
+		this.playCount 		= 0;
+		this.playMax 		= params.playMax || 1;
+		this.randomPlay		= params.randomPlay || false;
+		this.onEndFunct		= params.onEndFunct || false;
 	}
 
 	playSound()
 	{
+		this.main.addEventListener("ended", this.event_sound.bind(this), false);
 		this.main.play();
+	}
+
+	setRandDelay(hi, lo)
+	{
+		this.soundDelayHI = hi;
+		this.soundDelayLO = lo;
+	}
+
+	playSoundRandDelay()
+	{
+		this.soundDelay = Math.round(Math.random() * (this.soundDelayHI - this.soundDelayLO) + this.soundDelayLO);
+
+		this.delayTimer = setTimeout(this.playSound.bind(this), this.soundDelay * 1000);
+	}
+
+	event_sound(event)
+	{
+		if(event.type === "ended")
+		{
+			if(this.loop)
+			{
+				this.playCount ++;
+
+				if(this.playCount <= this.playMax)
+				{
+					if(this.randomPlay)
+					{
+						this.playSoundRandDelay();
+					}
+
+					else
+					{
+						this.playSound();
+					}
+
+					trace(this.playCount + " " + this.playMax);
+				}
+
+				else
+				{
+					this.loop = false;
+				}
+			}
+
+			if(this.onEndFunct)
+			{
+				this.onEndFunct();
+			}
+		}
 	}
 }
 
@@ -37,26 +89,16 @@ function sound_init()
 {
 	soundList = {};
 
-	// soundList.sfx_thunder = new SoundFX(document.querySelector(".sfx_thunder"));
-	// soundList.sfx_thunder.create({instanceClass: "sfx_thunder", loop: false, playCount: 0, playMax: 1});
-
-	// soundList.sfx_lightning = new SoundFX(document.querySelector(".sfx_lightning"));
-	// soundList.sfx_lightning.create({instanceClass: "sfx_lightning", loop: false, playCount: 0, playMax: 1});
-
-	// soundList.sfx_thunderClap = new SoundFX(document.querySelector(".sfx_thunderClap"));
-	// soundList.sfx_thunderClap.create({instanceClass: "sfx_thunderClap", loop: true, playCount: 0, playMax: 3});
-
-	sound_build({instanceClass: "sfx_thunder", loop: false, playCount: 0, playMax: 1});
-	sound_build({instanceClass: "sfx_lightning", loop: false, playCount: 0, playMax: 1});
-	sound_build({instanceClass: "sfx_thunderClap", loop: true, playCount: 0, playMax: 3});
+	sound_build({instanceClass: "sfx_thunder"});
+	sound_build({instanceClass: "sfx_lightning"});
+	sound_build({instanceClass: "sfx_thunderClap", loop: true, playMax: 3, randomPlay: true, onEndFunct: sound_test_d});
 
 	displayList = {};
 	displayList.go = document.querySelector(".go");
 
 	displayList.go.addEventListener("click", sound_test, false);
 
-
-	soundList.sfx_thunderClap.main.addEventListener("ended", sound_event, false);
+	soundList.sfx_thunderClap.setRandDelay(6, 3);
 }
 
 function sound_build(params)
@@ -68,7 +110,9 @@ function sound_build(params)
 function sound_test(event)
 {
 	displayList.go.remove();
-	timer = setTimeout(sound_test_a, 0.5 * 1000);
+	// timer = setTimeout(sound_test_a, 0.5 * 1000);
+
+	soundList.sfx_thunderClap.playSoundRandDelay();
 }
 
 
@@ -95,34 +139,12 @@ function sound_test_c()
 	soundList.sfx_thunderClap.playSound();
 }
 
-function sound_event(event)
+function sound_test_d()
 {
-	let soundTarget = event.target.className;
-	let soundObject;
-
-	
-	for(let i in soundList)
-	{
-		if(soundTarget === soundList[i].instanceClass)
-		{
-			soundObject = soundList[i];
-		}
-	}
-
-	if(soundObject.loop)
-	{
-		soundObject.playCount ++;
-
-		if(soundObject.playCount <= soundObject.playMax)
-		{
-			soundObject.playSound();
-
-			trace(soundObject.playCount + " " + soundObject.playMax);
-		}
-
-		else
-		{
-			soundObject.loop = false;
-		}
-	}
+	trace("fire");
+	soundList.sfx_lightning.playSound();
 }
+
+
+
+
